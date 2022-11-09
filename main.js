5be0d8b2755a9529d05051e1c82ecd792bccf1cf
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2022-11-09 16:26:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
 var heart 
 var outer = []
-var HEIGHT = 1000
-var WEIGHT = 600
-
+var HEIGHT = 1460
+var WEIGHT = 790
 var render_frame = 0
-
 function setup() {
     createCanvas(HEIGHT, WEIGHT)
     frameRate(10)
